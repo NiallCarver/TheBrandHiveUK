a60a1b3d0ca46db7de7b9ce9d946f8dc8c6c1e28
NiallCarver <niallcarver0131@gmail.com>
2025-10-20 00:28:55 +0100

    chore: add app\page.tsx

---
+++ app\page.tsx
@@ -0,0 +1,27 @@
+import Hero from '@/components/Hero'
+import Logos from '@/components/Logos'
+import Testimonials from '@/components/Testimonials'
+import Engagement from '@/components/Engagement'
+import Process from '@/components/Process'
+import InsightsTeaser from '@/components/InsightsTeaser'
+import Differentiators from '@/components/Differentiators'
+import FAQs from '@/components/FAQs'
+import CTA from '@/components/CTA'
+// import LeadForm from '@/components/LeadForm'
+// import Section from '@/components/Section'
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <Logos />
+      <Testimonials />
+      <Engagement />
+      <Process />
+      <InsightsTeaser />
+      <Differentiators />
+      <FAQs />
+      <CTA />
+    </>
+  )
+}
