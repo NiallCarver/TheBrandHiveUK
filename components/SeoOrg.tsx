85cddf5b98e539f11fa8445198a1441c8ba6c441
NiallCarver <niallcarver0131@gmail.com>
2025-10-20 04:08:16 +0100

    chore: update components/SeoOrg.tsx

---
+++ components/SeoOrg.tsx
@@ -0,0 +1,30 @@
+export default function SeoOrg(){
+  const org = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Brand Hive UK',
+    url: 'https://brandhive.uk',
+    logo: 'https://brandhive.uk/logos/The_Brand_Hive_UK_Transparent.png',
+    sameAs: ['https://x.com/', 'https://www.linkedin.com/'],
+  }
+  const website = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Brand Hive UK',
+    url: 'https://brandhive.uk',
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: 'https://brandhive.uk/?q={search_term_string}',
+      'query-input': 'required name=search_term_string',
+    },
+  }
+  return (
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(org) }} />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(website) }} />
+    </>
+  )
+}
+
+
+
